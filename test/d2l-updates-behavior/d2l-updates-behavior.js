22fbdf94df33359920ecbffa74e0298babf43ca1
Chester Twomey <chester.twomey@gmail.com>
2017-01-18 16:19:45 -0600

    update to consume configuration from external sources

--- test/d2l-updates-behavior/d2l-updates-behavior.js
+++ test/d2l-updates-behavior/d2l-updates-behavior.js
@@ -26,6 +26,13 @@ describe('d2l-updates-behavior', function() {
 				'UngradedQuizzes': 1
 			}],
 			'Next': null
+		},
+		updatesConfig = {
+			'showUnattemptedQuizzes': true,
+			'showDropboxUnreadFeedback': true,
+			'showUngradedQuizAttempts': true,
+			'showUnreadDiscussionMessages': true,
+			'showUnreadDropboxSubmissions': true
 		};
 
 	beforeEach(function() {
@@ -41,20 +48,34 @@ describe('d2l-updates-behavior', function() {
 
 	describe('getUpdates', function() {
 
-		it('calling get updates with a csv list of orgunitids should return a list of aggregated updates', function() {
-			server.respondWith('GET', component._updatesURI + '6609,6613', JSON.stringify(updates));
+		it('calling get updates with a csv list of orgunitids should return a list of aggregated updates', function(done) {
+			server.respondWith('GET', '/d2l/api/le/1.18/updates/myUpdates/?orgUnitIdsCSV=6609,6613', JSON.stringify(updates));
+			component.courseUpdatesConfig = updatesConfig;
 			component.getUpdates('6609,6613');
 			setTimeout(function() {
-				expect(component.get('updateCountMap')[6609]).to.equal(2);
-				expect(component.get('updateCountMap')[6613]).to.equal(6);
+				expect(component.updateCountMap[6609]).to.equal(2);
+				expect(component.updateCountMap[6613]).to.equal(6);
+				done();
 			}, 200);
 		});
 
-		it('should do nothing if a bad response comes back', function() {
-			server.respondWith('GET', component._updatesURI + '6609,6613', [500, {}, '']);
+		it('should do nothing if no configuration', function(done) {
+			server.respondWith('GET', '/d2l/api/le/1.18/updates/myUpdates/?orgUnitIdsCSV=6609,6613', JSON.stringify(updates));
+			component.getUpdates('6609,6613');
+			setTimeout(function() {
+				expect(component.updateCountMap[6609]).to.be.undefined;
+				expect(component.updateCountMap[6613]).to.be.undefined;
+				done();
+			}, 200);
+		});
+
+		it('should do nothing if a bad response comes back', function(done) {
+			server.respondWith('GET', '/d2l/api/le/1.18/updates/myUpdates/?orgUnitIdsCSV=6609,6613', [500, {}, '']);
+			component.courseUpdatesConfig = updatesConfig;
 			component.getUpdates('6609,6613');
 			setTimeout(function() {
 				expect(component.updateCountMap).to.be.empty;
+				done();
 			}, 200);
 
 		});
@@ -64,6 +85,12 @@ describe('d2l-updates-behavior', function() {
 		it('should generate a good uri if given a string', function() {
 			expect(component._generateUpdatesUri('6609,6613')).to.equal('/d2l/api/le/1.18/updates/myUpdates/?orgUnitIdsCSV=6609,6613');
 			expect(component._generateUpdatesUri('6609')).to.equal('/d2l/api/le/1.18/updates/myUpdates/?orgUnitIdsCSV=6609');
+		});
+
+		it('should do nothing if given non string values', function() {
+			expect(component._generateUpdatesUri()).to.be.undefined;
+			expect(component._generateUpdatesUri(123)).to.be.undefined;
+			expect(component._generateUpdatesUri({'garbage':'garbage'})).to.be.undefined;
 		});
 	});
 });

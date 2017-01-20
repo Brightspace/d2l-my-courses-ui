fa5a62d8029b81ca15af3e575bc55d5698144c14
Chester Twomey <chester.twomey@gmail.com>
2017-01-20 13:46:09 -0600

    adding test to break on missing locale info

--- test/localize-behavior/localize-behavior.js
+++ test/localize-behavior/localize-behavior.js
@@ -25,4 +25,18 @@ describe('localize behavior', function() {
 		expect(component.localize('allCourses')).to.equal('All Courses');
 	});
 
+	describe('localize mappings', function() {
+		it('should have translation for every english term', function() {
+			component = fixture('default-fixture');
+			var terms = Object.keys(component.resources['en']);
+			var locales = Object.keys(component.resources);
+			for (var i = 0; i < locales.length; i++) {
+				var currentLocale = locales[i];
+				for (var j = 0; j < terms.length; j++) {
+					expect(component.resources[currentLocale].hasOwnProperty(terms[j]), 'missing term ' + terms[j] + ' on locale ' + currentLocale).to.be.true;
+				}
+			}
+		});
+	});
+
 });

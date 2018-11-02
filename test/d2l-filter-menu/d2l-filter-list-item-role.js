874fce0b160b3c15a26499628f2fb59c4e77ff28
Noel Stonehouse <noel.stonehouse@d2l.com>
2018-11-02 09:43:29 -0400

    US101469: Wrap all tests in a describe. Trying to reduce flakiness in sauce tests.

--- test/d2l-filter-menu/d2l-filter-list-item-role.js
+++ test/d2l-filter-menu/d2l-filter-list-item-role.js
@@ -1,17 +1,13 @@
-var listItem;
-
-beforeEach(function() {
-	listItem = fixture('d2l-filter-list-item-role-fixture');
-});
-
 describe('d2l-filter-list-item-role', function() {
 	it('should show the unchecked icon when the item is not selected', function() {
+		var listItem = fixture('d2l-filter-list-item-role-fixture');
 		listItem.selected = false;
 		expect(listItem.$$('d2l-icon.icon-checked').getComputedStyleValue('display')).to.equal('none');
 		expect(listItem.$$('d2l-icon.icon-unchecked').getComputedStyleValue('display')).to.not.equal('none');
 	});
 
 	it('should show the checked icon when the item is selected', function() {
+		var listItem = fixture('d2l-filter-list-item-role-fixture');
 		listItem.selected = true;
 		expect(listItem.$$('d2l-icon.icon-unchecked').getComputedStyleValue('display')).to.equal('none');
 		expect(listItem.$$('d2l-icon.icon-checked').getComputedStyleValue('display')).to.not.equal('none');

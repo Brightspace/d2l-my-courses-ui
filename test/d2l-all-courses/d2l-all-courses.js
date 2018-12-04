30b1973739978f45e32ea9697612fb9f82282cf8
Mark Weiss <Mark.Weiss@D2L.com>
2018-12-04 13:45:40 -0500

    Lint fix.

--- test/d2l-all-courses/d2l-all-courses.js
+++ test/d2l-all-courses/d2l-all-courses.js
@@ -227,7 +227,6 @@ describe('d2l-all-courses', function() {
 		var semestersTabStub = sandbox.stub(widget.$.filterMenu.$.semestersTab, 'load');
 		var departmentsTabStub = sandbox.stub(widget.$.filterMenu.$.departmentsTab, 'load');
 
-
 		return widget._onFilterDropdownOpen().then(function() {
 			expect(semestersTabStub.called).to.be.true;
 			expect(departmentsTabStub.called).to.be.true;

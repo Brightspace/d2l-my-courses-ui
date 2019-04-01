6aece06e1414748e2af7cc7e69903ccd0d633475
Cheick Cisse <Cheick.Cisse@D2L.com>
2019-04-01 16:26:55 -0400

    Use EntitySTore Fetch

--- src/d2l-all-courses.js
+++ src/d2l-all-courses.js
@@ -398,9 +398,11 @@ Polymer({
 				this._enrollmentsSearchUrl = lastResponseEntity.getLinkByRel('next').href;
 				this.$.lazyLoadSpinner.scrollIntoView();
 
-				return this.fetchSirenEntity(this._enrollmentsSearchUrl)
+				return window.D2L.Siren.EntityStore.fetch(this._enrollmentsSearchUrl, this.token)
 					.then(function(enrollmentsEntity) {
-						this._updateFilteredEnrollments(enrollmentsEntity, true);
+						console.log(enrollmentsEntity);
+						console.log(enrollmentsEntity.entity);
+						this._updateFilteredEnrollments(enrollmentsEntity.entity, true);
 					}.bind(this));
 			}
 		}

89fcadd03a649a660b77d6b546fc031f440820f1
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-04-05 16:35:25 -0400

    Remove some more

--- build/lang/en.js
+++ build/lang/en.js
@@ -9,10 +9,5 @@ export const en = {
 	"pinnedCourses": "Pinned",
 	"unpinnedCourses": "Unpinned",
 	"closeSimpleOverlayAltText": "Close Dialog",
-	"pin": "Pin",
-	"pinActionResult": "{course} has been pinned",
-	"unpin": "Unpin",
-	"unpinActionResult": "{course} has been unpinned",
-	"noCoursesMessage": "You don't have any courses to display.",
-	"noPinnedCoursesWidgetMessage": "You don't have any pinned courses. View All Courses to browse all available courses."
+	"pin": "Pin"
 };

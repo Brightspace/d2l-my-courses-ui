3c681e0223505ba9650108a19d7a08f15ff76892
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-04-05 15:55:38 -0400

    More trimming

--- build/lang/en.js
+++ build/lang/en.js
@@ -1,55 +1,23 @@
 
-/* eslint-disable quotes, indent */
+/* eslint-disable quotes */
 window.D2L = window.D2L || {};
 window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
 window.D2L.PolymerBehaviors.MyCourses = window.D2L.PolymerBehaviors.MyCourses || {};
-/**
- * This file is auto-generated. Do not modify.
- * @polymerBehavior D2L.PolymerBehaviors.MyCourses.LangEnBehavior
- */
+
 D2L.PolymerBehaviors.MyCourses.LangEnBehavior = {
 	'en': {
-			"advancedSearch": "Advanced Search",
-			"viewAllCourses": "View All Courses",
-			"allTab": "All",
-			"allCourses": "All Courses",
-			"pinnedCourses": "Pinned",
-			"unpinnedCourses": "Unpinned",
-			"closeSimpleOverlayAltText": "Close Dialog",
-			"pin": "Pin",
-			"pinActionResult": "{course} has been pinned",
-			"unpin": "Unpin",
-			"unpinActionResult": "{course} has been unpinned",
-			"noCoursesMessage": "You don't have any courses to display.",
-			"noPinnedCoursesWidgetMessage": "You don't have any pinned courses. View All Courses to browse all available courses.",
-			"onlyPastCoursesMessage": "You don't have any current or future courses. View All Courses to browse your past courses.",
-			"noPinnedCoursesMessage": "You don't have any pinned courses. Pin your favorite courses to make them easier to find.",
-			"noUnpinnedCoursesMessage": "Nothing to see here! All of your courses are pinned.",
-			"noPinnedCoursesInSearch": "There are no pinned courses for your search entry.",
-			"noUnpinnedCoursesInSearch": "There are no unpinned courses for your search entry.",
-			"noPinnedCoursesInSelection": "There are no pinned courses for your selection.",
-			"noUnpinnedCoursesInSelection": "There are no unpinned courses for your selection.",
-			"noCoursesInSearch": "There are no courses for your search entry.",
-			"noCoursesInSelection": "There are no courses for your selection.",
-			"noCoursesInDepartment": "There are no current or active courses in this department",
-			"noCoursesInSemester": "There are no current or active courses in this semester",
-			"noCoursesInRole": "There are no current or active courses for this role",
-			"noPinnedCoursesInDepartment": "There are no current or active pinned courses in this department",
-			"noPinnedCoursesInSemester": "There are no current or active pinned courses in this semester",
-			"noPinnedCoursesInRole": "There are no current or active pinned courses for this role",
-			"noUnpinnedCoursesInDepartment": "There are no current or active unpinned courses in this department",
-			"noUnpinnedCoursesInSemester": "There are no current or active unpinned courses in this semester",
-			"noUnpinnedCoursesInRole": "There are no current or active unpinned courses for this role",
-			"search": "Search",
-			"upload": "Upload",
-			"noSearchResults": "No results.",
-			"courseSearchInputPlaceholder": "Find a course",
-			"recentSearches": "Recent Searches",
-			"changeImage": "Change Image",
-			"useThisImage": "Use this image",
-			"courseSettings": "{course} course settings",
-			"courseOfferingInformation": "Course Offering Information",
-			"startedInactiveAlert": "You have courses that have started but are set as inactive. Users may not be able to access these courses.",
-			"startedInactiveAlertPinned": "You have pinned courses that have started but are set as inactive. Users may not be able to access these courses."
-}
+		"advancedSearch": "Advanced Search",
+		"viewAllCourses": "View All Courses",
+		"allTab": "All",
+		"allCourses": "All Courses",
+		"pinnedCourses": "Pinned",
+		"unpinnedCourses": "Unpinned",
+		"closeSimpleOverlayAltText": "Close Dialog",
+		"pin": "Pin",
+		"pinActionResult": "{course} has been pinned",
+		"unpin": "Unpin",
+		"unpinActionResult": "{course} has been unpinned",
+		"noCoursesMessage": "You don't have any courses to display.",
+		"noPinnedCoursesWidgetMessage": "You don't have any pinned courses. View All Courses to browse all available courses."
+	}
 };
